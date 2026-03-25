87dd533ddb216ab3b3cbbeee70b9153657897454
USAMA ASHRAF <usamaashraf728@gmail.com>
2026-03-25 19:27:07 +0500

    ,,

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/tailadmin-nextjs' : '';
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   trailingSlash: true,
-  basePath: basePath,
-  assetPrefix: basePath,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   webpack(config) {
     config.module.rules.push({
